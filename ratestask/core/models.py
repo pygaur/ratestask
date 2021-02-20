c5a3a3a5f5a26f01496a2f139d2aff7921852798
Prashant Gaur <91prashantgaur@gmail.com>
2021-02-21 01:42:19 +0530

    added DB models and migration file

--- ratestask/core/models.py
+++ ratestask/core/models.py
@@ -1,3 +1,48 @@
+"""
+"""
 from django.db import models
 
-# Create your models here.
+
+class Region(models.Model):
+    """
+    """
+    slug = models.SlugField(primary_key=True)
+    name = models.TextField()
+    # below column name could be parent
+    parent = models.ForeignKey('self', models.DO_NOTHING,
+                               db_column='parent_slug',
+                               blank=True, null=True)
+
+    class Meta:
+        db_table = 'regions'
+
+
+class Port(models.Model):
+    """
+    """
+    code = models.TextField(primary_key=True, max_length=5)
+    name = models.TextField()
+    # below column name could be region
+    region = models.ForeignKey('Region', models.DO_NOTHING,
+                               db_column='parent_slug')
+
+    class Meta:
+        db_table = 'ports'
+
+
+class Price(models.Model):
+    orig_code = models.ForeignKey(Port, models.DO_NOTHING,
+                                  related_name='orig_code',
+                                  max_length=5, db_column='orig_code')
+    dest_code = models.ForeignKey(Port, models.DO_NOTHING,
+                                  related_name='dest_code',
+                                  max_length=5, db_column='dest_code')
+    day = models.DateField(db_column='day')
+    # changing column type to decimal
+    price = models.DecimalField(max_digits=8, decimal_places=2, db_column='price')
+
+    class Meta:
+        db_table = 'prices'
+
+
+
